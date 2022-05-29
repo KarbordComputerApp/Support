157f3b10bdc6b285a5ed591ac1dede12609b5731
KarbordComputerApp <62652601+KarbordComputerApp@users.noreply.github.com>
2022-05-29 15:27:28 +0430

    رفع اشکال

--- Support/Controllers/Unit/UnitPublic.cs
+++ Support/Controllers/Unit/UnitPublic.cs
@@ -15,7 +15,7 @@ namespace Support.Controllers.Unit
     public class UnitPublic
     {
        public static string titleVer = "ورژن تست";
-       public static string titleVerNumber = "14"; 
+       public static string titleVerNumber = "15"; 
 
         //public static string titleVer = "ورژن";
         //public static string titleVerNumber = "1001";
--- Support/Scripts/User.js
+++ Support/Scripts/User.js
@@ -1,6 +1,9 @@
 ﻿var ViewModel = function () {
     $("#Index_TextLogo").text('مشخصات شرکت');
 
+    $("#LName").val(fullName);
+    $("#EMail").val(localStorage.getItem("Email"));
+
 
        
 };
