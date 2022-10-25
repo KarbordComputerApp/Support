e5a52ef3f273448633ccef6c0c10521753344ee1
karbord <karbord@SERVER>
2022-10-25 14:24:40 +0330

    رفع اشکال

--- Support/Controllers/Unit/UnitPublic.cs
+++ Support/Controllers/Unit/UnitPublic.cs
@@ -15,7 +15,7 @@ namespace Support.Controllers.Unit
     public class UnitPublic
     {
        public static string titleVer = "ورژن تست";
-       public static string titleVerNumber = "31"; 
+       public static string titleVerNumber = "32"; 
 
         //public static string titleVer = "ورژن";
         //public static string titleVerNumber = "1001";
