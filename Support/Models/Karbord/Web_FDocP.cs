5f3afdf8dce68cca2c43fd979dc84499ae0abc0a
karbord <karbord@SERVER>
2023-01-11 11:51:06 +0330

    رفع اشکال

--- Support/Models/Karbord/Web_FDocP.cs
+++ Support/Models/Karbord/Web_FDocP.cs
@@ -226,5 +226,44 @@ namespace Support.Models
 
         public double? AddMin10 { get; set; }
 
+        public string F01 { get; set; }
+
+        public string F02 { get; set; }
+
+        public string F03 { get; set; }
+
+        public string F04 { get; set; }
+
+        public string F05 { get; set; }
+
+        public string F06 { get; set; }
+
+        public string F07 { get; set; }
+
+        public string F08 { get; set; }
+
+        public string F09 { get; set; }
+
+        public string F10 { get; set; }
+
+        public string F11 { get; set; }
+
+        public string F12 { get; set; }
+
+        public string F13 { get; set; }
+
+        public string F14 { get; set; }
+
+        public string F15 { get; set; }
+
+        public string F16 { get; set; }
+
+        public string F17 { get; set; }
+
+        public string F18 { get; set; }
+
+        public string F19 { get; set; }
+
+        public string F20 { get; set; }
     }
 }
