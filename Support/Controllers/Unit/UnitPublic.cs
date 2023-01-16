0994a5bc63a000c99d659258f7082848e754ddef
karbord <karbord@SERVER>
2023-01-16 15:14:43 +0330

    افزودن متد دانلود

--- Support/Controllers/Unit/UnitPublic.cs
+++ Support/Controllers/Unit/UnitPublic.cs
@@ -16,7 +16,7 @@ namespace Support.Controllers.Unit
     public class UnitPublic
     {
         public static string titleVer = "ورژن تست";
-        public static string titleVerNumber = "72";
+        public static string titleVerNumber = "73";
 
         //public static string titleVer = "ورژن";
         //public static string titleVerNumber = "1001";
