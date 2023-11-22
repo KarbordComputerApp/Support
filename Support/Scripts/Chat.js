2e670d705966f7b712d8d3125c09c80bce84f2bd
admin <admin@SERVER0>
2023-11-22 11:26:49 +0330

    افزودن چت

---
+++ Support/Scripts/Chat.js
@@ -0,0 +1,12 @@
+﻿$("#box-chat").hide();
+$("#chat-bell").show();
+$("#chat-bell-image-wrapper").click(function () {
+    $("#chat-bell").hide();
+    $("#box-chat").show();
+});
+
+$("#btn-close-chat").click(function () {
+    $("#chat-bell").show();
+    $("#box-chat").hide();
+});
+
